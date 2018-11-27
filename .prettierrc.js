f053a63ac3d493e070ac06de0de45502f29ef56e
sunxu <ntsunxu@163.com>
2018-11-27 21:10:25 +0800

    install prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    trailingComma: 'none',
+    wrapAttributes: true
+}
