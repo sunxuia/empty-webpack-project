aa1b1871718a9119ea56b1f078fb91a3158b26c2
sunxu <ntsunxu@163.com>
2018-11-28 18:46:33 +0800

    set css module import

--- build/utils.js
+++ build/utils.js
@@ -15,11 +15,14 @@ exports.assetsLoader = function ({ test, outputPath, ...options }) {
     return {
         test,
         loader: 'url-loader',
-        options: Object.assign({
-            limit: 1000,
-            name: '[name].[hash:7].[ext]',
-            fallback: 'file-loader'
-        }, options)
+        options: Object.assign(
+            {
+                limit: 1000,
+                name: '[name].[hash:7].[ext]',
+                fallback: 'file-loader'
+            },
+            options
+        )
     }
 }
 
@@ -35,23 +38,40 @@ exports.styleLoader = function ({ test, loader, extract, ...options }) {
             })
         }
     }
-    const loaders = [
-        extract ? MiniCssExtractPlugin.loader
-            : generateLoader('style-loader'),
-        generateLoader('css-loader', {
-            modules: true,
-            camelCase: 'dashes',
-            localIdentName: '[name]-[local]__[hash:4]',
-            importLoaders: loader ? 2 : 1,
-            limit: 1000
-        }),
-        generateLoader('postcss-loader')
-    ]
-    if (loader) {
-        loaders.push(generateLoader(loader, options))
+
+    function generateStyleLoaders (cssLoaderOptions) {
+        const res = [
+            extract ? MiniCssExtractPlugin.loader : generateLoader('style-loader'),
+            generateLoader(
+                'css-loader',
+                Object.assign(
+                    {
+                        importLoaders: loader ? 2 : 1,
+                        limit: 1000
+                    },
+                    cssLoaderOptions
+                )
+            )
+        ]
+        if (loader) {
+            res.push(generateLoader(loader, options))
+        }
+        return res
     }
     return {
         test,
-        use: loaders
+        oneOf: [
+            {
+                resourceQuery: /module/,
+                use: generateStyleLoaders({
+                    modules: true,
+                    camelCase: 'dashes',
+                    localIdentName: '[name]-[local]__[hash:4]'
+                })
+            },
+            {
+                use: generateStyleLoaders()
+            }
+        ]
     }
 }
